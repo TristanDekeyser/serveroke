0730ab7183e38ecca04a972645c28749f50313e6
Tristan Dekeyser <tristan.dekeyser@gmail.com>
2023-10-27 10:04:50 +0200

    toegankelijk maken via terminal aan de hand van sys.argv

--- serverchecker.py
+++ serverchecker.py
@@ -1,8 +1,19 @@
-ant = input("wil je servers toevoegen, verwijderen of een lijst laten zien? (a/b/c)")
-match ant:
-    case "a":
-        print ("u heef gekozen om een server toe te voegen")
-    case "b":
-        print ("u heeft gekozen om een server te verwijderen")
-    case "c":
-        print ("u heef gekozen om de server lijst te laten zien")
+import sys
+if __name__=='__main__':
+    if len(sys.argv) > 1:
+        match sys.argv[1]:
+            case "a":
+                print ("u heef gekozen om een server toe te voegen")
+            case "b":
+                print ("u heeft gekozen om een server te verwijderen")
+            case "c":
+                print ("u heef gekozen om de server lijst te laten zien")
+    else:
+        ant = input("wil je servers toevoegen, verwijderen of een lijst laten zien? (a/b/c)")
+        match ant:
+            case "a":
+                print ("u heef gekozen om een server toe te voegen")
+            case "b":
+                print ("u heeft gekozen om een server te verwijderen")
+            case "c":
+                print ("u heef gekozen om de server lijst te laten zien")
