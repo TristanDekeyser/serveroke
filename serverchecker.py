1271c9e17bcea87b626d857090d27951490fbbf9
Tristan Dekeyser <tristan.dekeyser@gmail.com>
2023-10-27 11:13:28 +0200

    server lijst toevoegen en laten zien

--- serverchecker.py
+++ serverchecker.py
@@ -9,35 +9,51 @@ def naar_Json2(letter, server):
     with open("keuzes.json", "a") as f2:
         f2.write(json.dumps({"keuze": letter, "server address": server}, sort_keys = True))
 
+def list_servers(servers):
+    for index, server in enumerate(servers):
+        print(f"{index}: {server}")
+
+def gui():
+    blijven = True
+    while blijven:
+        ant = input("wil je servers toevoegen, verwijderen of een lijst laten zien? (a/b/c) (stoppen = 0)")
+        match ant:
+            case "a":
+                print ("u heef gekozen om een server toe te voegen")
+                server = input("geef een server")
+                servers.append(server)
+                naar_Json2(ant, server)
+            case "b":
+                print ("u heeft gekozen om een server te verwijderen")
+                server = input("geef een server")
+                servers.remove(server)
+                naar_Json2(ant, server)
+            case "c":
+                print ("u heef gekozen om de server lijst te laten zien")
+                list_servers(servers)
+            case "0":
+                blijven = False
+            case _:
+                print ("dit is geen geldige optie")
+
 servers = []
 if __name__=='__main__':
     if len(sys.argv) > 1:
         match sys.argv[1]:
             case "a":
                 print ("u heef gekozen om een server toe te voegen")
-                servers.append(sys.argv[1])
+                servers.append(sys.argv[2])
+                naar_Json()
             case "b":
                 print ("u heeft gekozen om een server te verwijderen")
-                servers.remove(sys.argv[1])
+                servers.remove(sys.argv[2])
+                naar_Json()
             case "c":
                 print ("u heef gekozen om de server lijst te laten zien")
+                list_servers(servers)
             case _:
                 print ("dit is geen geldige optie")
-        naar_Json()
-
+        gui()
     else:
-        ant = input("wil je servers toevoegen, verwijderen of een lijst laten zien? (a/b/c)")
-        match ant:
-            case "a":
-                print ("u heef gekozen om een server toe te voegen")
-                server = input("geef een server")
-                servers.append(server)
-            case "b":
-                print ("u heeft gekozen om een server te verwijderen")
-                server = input("geef een server")
-                servers.remove(server)
-            case "c":
-                print ("u heef gekozen om de server lijst te laten zien")
-            case _:
-                print ("dit is geen geldige optie")
-        naar_Json2(ant, server)
+        gui()
+        
