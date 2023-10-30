5fb7e9e695cf8aa3a217c4a30b513509b9d0198c
Tristan Dekeyser <tristan.dekeyser@gmail.com>
2023-10-30 17:53:19 +0100

    management en check modus toegevoegd

--- serverchecker.py
+++ serverchecker.py
@@ -1,14 +1,6 @@
 import sys
 import json
 
-# def naar_Json():
-#     with open("keuzes.json", "a") as f2:
-#         f2.write(json.dumps({"keuze": sys.argv[1], "server address": sys.argv[2]}, sort_keys = True))
-
-# def naar_Json2(letter, server):
-#     with open("keuzes.json", "a") as f2:
-#         f2.write(json.dumps({"keuze": letter, "server address": server}, sort_keys = True))
-
 def update_json():
     with open("keuzes.json", "w") as f:
         f.write(json.dumps(servers))
@@ -17,6 +9,20 @@ def update_json():
 def list_servers(servers):
     for index, server in enumerate(servers):
         print(f"{index}: {server}")
+
+def keuze():
+    if len(sys.argv) > 3:
+        if sys.argv[3] == "man":
+            cli()
+        elif sys.argv[3] == "checks":
+            checks()
+    elif len(sys.argv) == 2:
+        if sys.argv[1] == "man":
+            menu()
+        elif sys.argv[1] == "checks":
+            checks()
+    elif len(sys.argv) == 1:
+        menu()
 
 def menu():
     blijven = True
@@ -28,13 +34,11 @@ def menu():
                 server = input("geef een server: ")
                 servers.append(server)
                 update_json()
-                # naar_Json2(ant, server)
             case "b":
                 print ("u heeft gekozen om een server te verwijderen")
                 server = input("geef een server: ")
                 servers.remove(server)
                 update_json()
-                # naar_Json2(ant, server)
             case "c":
                 print ("u heef gekozen om de server lijst te laten zien")
                 list_servers(servers)
@@ -49,11 +53,9 @@ def cli():
             print ("u heef gekozen om een server toe te voegen")
             servers.append(sys.argv[2])
             update_json()
-            # naar_Json()
         case "b":
             print ("u heeft gekozen om een server te verwijderen")
             del servers[-1]
-            # naar_Json()
         case "c":
             print ("u heef gekozen om de server lijst te laten zien")
             list_servers(servers)
@@ -61,15 +63,15 @@ def cli():
             print ("dit is geen geldige optie")
     menu()
 
+def checks():
+    print("checks optie zal later komen")
+
 if __name__=='__main__':
     servers = []
     try:
         with open ("keuzes.json", "r") as f:
             servers = json.loads(f.read())
-        if len(sys.argv) > 1:
-            cli()
-        else:
-            menu()
+        keuze()
     except IOError:
         servers = []
         if len(sys.argv) > 1:
